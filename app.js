2b295fadb8edd3f77b34afd5fe75a7cc31c1277a
beamchristian <beamchristian@yahoo.com>
2021-10-11 20:03:26 -0400

    basic html generator setup

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [userName, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(userName, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
