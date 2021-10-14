bf4599d9a188b8d955179d6f0a1d020b7614c4c0
beamchristian <beamchristian@yahoo.com>
2021-10-14 12:24:55 -0400

    Refactored code to promises

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -136,15 +136,26 @@ Add a New Project
 };
 
 promptUser()
+  // asking the user for their information with Inquirer prompts; this returns all of the data as an object in a Promise
   .then(promptProject)
+  // Each project will be pushed into a projects array in the collection of portfolio information, and when we're done, the final set of data is returned to the next .then().
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-      console.log(
-        'Page created! Check out index.html in this directory to see it!'
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  // The finished portfolio data object is returned as portfolioData and sent into the generatePage() function, which will return the finished HTML template code into pageHTML.
+  .then(pageHTML => {
+    // We pass pageHTML into the newly created writeFile() function, which returns a Promise. This is why we use return here, so the Promise is returned into the next .then() method.
+    return writeFile(pageHTML);
+  })
+  // Upon a successful file creation, we take the writeFileResponse object provided by the writeFile() function's resolve() execution to log it, and then we return copyFile().
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  // The Promise returned by copyFile() then lets us know if the CSS file was copied correctly, and if so, we're all done!
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
